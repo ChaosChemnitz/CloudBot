2a3c9f52dca393f2d2e29ced32e652c8b967162c
root <root@vcup.florz.net>
2016-02-04 16:53:15 +0100

    added logging to syslog

--- restartcloudbot.sh
+++ restartcloudbot.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
-
+#!/bin/bash
 cd $(dirname $0)
-while /bin/true;
+
+PIDS=""
+for i in $(ps -awx | grep -i [c]loudbot | sed "s/^\s*//" | cut -d" " -f1); do
+	if [ $i != $$ ]; then
+		PIDS="$i $PIDS"
+	fi
+done
+if [ ! -z "$PIDS" ]; then
+	kill $PIDS
+	sleep 10
+	kill -9 $PIDS >/dev/null 2>&1
+fi
+
+while true
 do
-	sudo -u cloudbot ./cloudbot.py
+	sudo -u cloudbot ./cloudbot.py 2>&1 | logger -s -t $(basename $0)
 	sleep 10
 done
 
