8941d59dc20a837d904de208ee559d166c13a0e1
ChChBot admin <kontakt@chaoschemnitz.de>
2017-02-24 17:08:01 +0100

    removed buggy part for killing old processes

--- restartcloudbot.sh
+++ restartcloudbot.sh
@@ -2,18 +2,6 @@
 cd $(dirname $0)
 
 (
-PIDS=""
-for i in $(ps -awx | grep -i [c]loudbot | sed "s/^\s*//" | cut -d" " -f1); do
-	if [ $i != $$ ]; then
-		PIDS="$i $PIDS"
-	fi
-done
-if [ ! -z "$PIDS" ]; then
-	kill $PIDS
-	sleep 10
-	kill -9 $PIDS >/dev/null 2>&1
-fi
-
 while true
 do
 	sudo -u cloudbot ./cloudbot.py 2>&1 | logger -s -t $(basename $0)
